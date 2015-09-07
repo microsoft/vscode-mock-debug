b4c90961ee0eb13c61c495eeaa086cef1b989543
Andre Weinand <aweinand@microsoft.com>
2015-09-07 12:57:56 +0200

    new setup for debug adapter development

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,22 @@ var gulp = require('gulp');
 var path = require('path');
 var tsb = require('gulp-tsb');
 var log = require('gulp-util').log;
-var git = require('git-rev-sync');
 var del = require('del');
 var runSequence = require('run-sequence');
 
 var compilation = tsb.create(path.join(__dirname, 'tsconfig.json'), true);
 
 var sources = [
-	'common/**/*.ts',
 	'mock/**/*.ts',
-	'typings/**/*.ts',
-	'test/**/*.ts'
+	'common/**/*.ts',
+	'typings/**/*.ts'
 ];
 
-var dest = 'out';
-var pluginDest = 'plugin/' + git.short();
+var dest = 'plugins/vs.debug.mock/debugAdapter';
+
+gulp.task('default', function(callback) {
+	runSequence('clean', 'build', callback);
+});
 
 gulp.task('build', function() {
 	return gulp.src(sources, { base: '.' })
@@ -28,28 +29,11 @@ gulp.task('build', function() {
 		.pipe(gulp.dest(dest));
 });
 
-gulp.task('copy-out', function() {
-	return gulp.src('out/**/*').pipe(gulp.dest(pluginDest + '/out'));
-});
-
-gulp.task('copy-node-modules', function() {
-	return gulp.src('node_modules/source-map/**/*').pipe(gulp.dest(pluginDest + '/node_modules/source-map'));
-});
-
-gulp.task('plugin-clean', function() {
-	return del('plugin/**/*');
+gulp.task('clean', function() {
+	return del('./plugins/vs.debug.mock/debugAdapter');
 })
 
 gulp.task('ts-watch', ['build'], function(cb) {
 	log('Watching build sources...');
     gulp.watch(sources, ['build']);
 });
-
-gulp.task('default', function(callback) {
-	runSequence('build', callback);
-});
-
-gulp.task('plugin-build', function(callback) {
-	runSequence(['build', 'plugin-clean'], ['copy-out', 'copy-node-modules'], callback);
-});
-
