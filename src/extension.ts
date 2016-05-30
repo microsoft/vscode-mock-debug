20c58595e3075758421829311c4184d484c5b24a
Andre Weinand <aweinand@microsoft.com>
2016-05-30 15:23:12 +0200

    remove dependency

--- src/extension.ts
+++ src/extension.ts
@@ -6,30 +6,68 @@
 
 import * as vscode from 'vscode';
 
-var ps = require('ps-node');
+var cp = require('child_process');
+
+class ProcessItem implements vscode.QuickPickItem {
+	label: string;
+	description: string;
+	// detail: string;
+	pid: string;
+}
+
+function listProcesses(args: string) : Promise<any[]> {
+
+	return new Promise((resolve, reject) => {
+		cp.exec('ps ' + args, function(err, stdout, stderr) {
+
+			if (err || stderr) {
+				reject( err || stderr.toString() );
+			} else {
+
+				const os = stdout.toString().split('\n');
+
+				let items : ProcessItem[]= [];
+
+				for (let i = 0; i < os.length; i++) {
+					const o : string = os[i];
+
+					const pos = o.indexOf(' ');
+					if (pos > 0) {
+						const pid = o.substr(0, pos);
+						const cmd = o.substr(pos+1);
+
+						items.push({ label: cmd, description: pid, pid: pid });
+					}
+				}
+
+				resolve(items);
+			}
+
+		});
+	});
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('extension.pickProcess', () => {
 
-		ps.lookup({
-			psargs: 'ax'
-		}, (err, resultList) => {
+		return listProcesses('-ax -c -o pid,command').then(items => {
 
-			let items = [];
+			let options : vscode.QuickPickOptions = {
+				placeHolder: "Pick the process to attach to",
+				matchOnDescription: true,
+				matchOnDetail: false
+			};
 
-			if (err) {
-				items.push(err.message);
-			} else {
-				resultList.forEach( process => {
-					if (process && process.command) {
-						items.push(`${process.command}`);
-					}
-				});
-			}
-
-			vscode.window.showQuickPick(items);
+			return vscode.window.showQuickPick(items, options).then(item => {
+				if (item) {
+					return item.pid;
+				} else {
+					return null;
+				}
+			});
 		});
+
 	});
 
 	context.subscriptions.push(disposable);
