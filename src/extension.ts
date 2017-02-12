ba12410835c21b2789fd096eac6294af21be83b0
Andre Weinand <aweinand@microsoft.com>
2017-02-12 23:54:56 +0100

    cosmetics

--- src/extension.ts
+++ src/extension.ts
@@ -9,24 +9,24 @@ import * as vscode from 'vscode';
 const initialConfigurations = {
 	version: '0.2.0',
 	configurations: [
-	{
-		type: 'mock',
-		request: 'launch',
-		name: 'Mock-Debug',
-		program: '${workspaceRoot}/${command.AskForProgramName}',
-		stopOnEntry: true
-	}
-]}
+		{
+			type: 'mock',
+			request: 'launch',
+			name: 'Mock Debug',
+			program: '${workspaceRoot}/readme.md',
+			stopOnEntry: true
+		}
+	]
+};
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('extension.mock-debug.getProgramName', config => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.mock-debug.getProgramName', config => {
 		return vscode.window.showInputBox({
 			placeHolder: "Please enter the name of a markdown file in the workspace folder",
 			value: "readme.md"
 		});
-	});
-	context.subscriptions.push(disposable);
+	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.mock-debug.provideInitialConfigurations', () => {
 		return [
@@ -38,4 +38,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
+	// nothing to do
 }
