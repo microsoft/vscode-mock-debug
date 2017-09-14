8e6baef7d6ad194cb03561def194e2036e565430
Andre Weinand <aweinand@microsoft.com>
2017-09-14 23:44:38 +0200

    Revert "use DebugConfigurationProvider"
    This reverts commit 9a81d45278cce7494768402308b63f14a93b2306.

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,21 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import {
-	DebugConfigurationProvider, DebugConfiguration,
-	WorkspaceFolder, CancellationToken, ProviderResult, ExtensionContext
-} from 'vscode';
 
+const initialConfigurations = {
+	version: '0.2.0',
+	configurations: [
+		{
+			type: 'mock',
+			request: 'launch',
+			name: 'Mock Debug',
+			program: '${workspaceRoot}/readme.md',
+			stopOnEntry: true
+		}
+	]
+};
 
-export function activate(context: ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.mock-debug.getProgramName', config => {
 		return vscode.window.showInputBox({
@@ -20,60 +28,15 @@ export function activate(context: ExtensionContext) {
 		});
 	}));
 
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mock', new MockDebugConfigurationProvider()));
+	context.subscriptions.push(vscode.commands.registerCommand('extension.mock-debug.provideInitialConfigurations', () => {
+		return [
+			'// Use IntelliSense to learn about possible Mock debug attributes.',
+			'// Hover to view descriptions of existing attributes.',
+			JSON.stringify(initialConfigurations, null, '\t')
+		].join('\n');
+	}));
 }
 
 export function deactivate() {
 	// nothing to do
-}
-
-class MockDebugConfigurationProvider implements DebugConfigurationProvider {
-
-	provideDebugConfigurations?(folder: WorkspaceFolder | undefined, token?: CancellationToken): ProviderResult<DebugConfiguration[]> {
-
-		return [
-			{
-				type: 'mock',
-				request: 'launch',
-				name: 'Mock Debug',
-				program: '${workspaceRoot}/readme.md',
-				stopOnEntry: true
-			},
-			{
-				type: 'mock',
-				request: 'launch',
-				name: 'Ask for file name',
-				program: '${workspaceRoot}/${command:AskForProgramName}',
-				stopOnEntry: true
-			}
-		];
-	}
-
-	resolveDebugConfiguration?(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
-
-		// make sure that all mandatory attributes have a value
-		config.type = config.type || 'mock';
-		config.request = config.request || 'launch';
-		config.name = config.name || 'Mock Debug';
-
-		// by default stop on entry
-		if (typeof config.stopOnEntry !== 'boolean') {
-			config.stopOnEntry = true;
-		}
-
-		if (!config.program) {
-			// try to use editor
-			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === 'markdown') {
-				config.program = editor.document.fileName;
-			}
-		}
-
-		if (!config.program) {
-			// still no program? ask for it!
-			config.program = '${workspaceRoot}/${command:AskForProgramName}';
-		}
-
-		return config;
-	}
 }
