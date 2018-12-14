e2cfc820dc269ff70f6d53badb9e31e6ac036a4d
Andre Weinand <aweinand@microsoft.com>
2018-12-14 11:50:56 +0100

    Use new DebugAdapterDescriptorFactory API

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ import * as Net from 'net';
 /*
  * Set the following compile time flag to true if the
  * debug adapter should run inside the extension host.
- * Please note: the test suite does no longer work in this mode.
+ * Please note: the test suite does not (yet) work in this mode.
  */
 const EMBED_DEBUG_ADAPTER = false;
 
@@ -29,70 +29,70 @@ export function activate(context: vscode.ExtensionContext) {
 	const provider = new MockConfigurationProvider();
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mock', provider));
 
-    if (EMBED_DEBUG_ADAPTER) {
-        const factory = new MockDebugAdapterDescriptorFactory();
-        context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('mock', factory));
-        context.subscriptions.push(factory);
-    }
+	if (EMBED_DEBUG_ADAPTER) {
+		const factory = new MockDebugAdapterDescriptorFactory();
+		context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('mock', factory));
+		context.subscriptions.push(factory);
+	}
 }
 
 export function deactivate() {
-    // nothing to do
+	// nothing to do
 }
 
 
 class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
 
-    /**
-     * Massage a debug configuration just before a debug session is being launched,
-     * e.g. add all missing attributes to the debug configuration.
-     */
-    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
 
-        // if launch.json is missing or empty
-        if (!config.type && !config.request && !config.name) {
-            const editor = vscode.window.activeTextEditor;
-            if (editor && editor.document.languageId === 'markdown') {
-                config.type = 'mock';
-                config.name = 'Launch';
-                config.request = 'launch';
-                config.program = '${file}';
-                config.stopOnEntry = true;
-            }
-        }
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'markdown') {
+				config.type = 'mock';
+				config.name = 'Launch';
+				config.request = 'launch';
+				config.program = '${file}';
+				config.stopOnEntry = true;
+			}
+		}
 
-        if (!config.program) {
-            return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
-                return undefined;	// abort launch
-            });
-        }
+		if (!config.program) {
+			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+				return undefined;	// abort launch
+			});
+		}
 
-        return config;
-    }
+		return config;
+	}
 }
 
 class MockDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
-    private server?: Net.Server;
+	private server?: Net.Server;
 
-    createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
 
-        if (!this.server) {
-            // start listening on a random port
-            this.server = Net.createServer(socket => {
-                const session = new MockDebugSession();
-                session.setRunAsServer(true);
-                session.start(<NodeJS.ReadableStream>socket, socket);
-            }).listen(0);
-        }
+		if (!this.server) {
+			// start listening on a random port
+			this.server = Net.createServer(socket => {
+				const session = new MockDebugSession();
+				session.setRunAsServer(true);
+				session.start(<NodeJS.ReadableStream>socket, socket);
+			}).listen(0);
+		}
 
-        // make VS Code connect to debug server
-        return new vscode.DebugAdapterServer(this.server.address().port);
-    }
+		// make VS Code connect to debug server
+		return new vscode.DebugAdapterServer(this.server.address().port);
+	}
 
-    dispose() {
-        if (this.server) {
-            this.server.close();
-        }
-    }
+	dispose() {
+		if (this.server) {
+			this.server.close();
+		}
+	}
 }
