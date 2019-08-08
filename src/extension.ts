48b05125b034f30f1017bede2b31058f7bb4ab02
Andre Weinand <aweinand@microsoft.com>
2019-08-08 16:05:17 +0200

    update dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -87,7 +87,7 @@ class MockDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptor
 		}
 
 		// make VS Code connect to debug server
-		return new vscode.DebugAdapterServer(this.server.address().port);
+		return new vscode.DebugAdapterServer((<Net.AddressInfo>this.server.address()).port);
 	}
 
 	dispose() {
