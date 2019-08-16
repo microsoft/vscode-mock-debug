e04d0030b1257d11d411759569d496bb66e16325
Andre Weinand <aweinand@microsoft.com>
2019-08-16 18:17:15 +0200

    set canPersist to true

--- src/mockDebug.ts
+++ src/mockDebug.ts
@@ -314,6 +314,7 @@ export class MockDebugSession extends LoggingDebugSession {
 				response.body.dataId = args.name;
 				response.body.description = args.name;
 				response.body.accessTypes = [ "read" ];
+				response.body.canPersist = true;
 			}
 		}
 
