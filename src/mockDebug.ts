bde9822a873878a68529e20afb95860aadd5ea8e
Andre Weinand <aweinand@microsoft.com>
2019-12-12 11:39:33 +0100

    make VS Code persist data breakpoints

--- src/mockDebug.ts
+++ src/mockDebug.ts
@@ -381,7 +381,7 @@ export class MockDebugSession extends LoggingDebugSession {
 				response.body.dataId = args.name;
 				response.body.description = args.name;
 				response.body.accessTypes = [ "read" ];
-				response.body.canPersist = false;
+				response.body.canPersist = true;
 			}
 		}
 
