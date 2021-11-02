b9bfbb015b91f4638b1ba42714d4d88886573a0c
Andre Weinand <aweinand@microsoft.com>
2021-11-02 15:57:27 +0100

    simplify and align webpack configs

--- build/extension.webpack.config.js
+++ build/extension.webpack.config.js
@@ -8,45 +8,34 @@
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
-
-const path = require('path');
-
 module.exports = /** @type WebpackConfig */ {
-	context: path.dirname(__dirname),
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-	target: 'node', // vscode extensions run in a Node.js-context
-	entry: {
-		extension: './src/extension.ts'
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+	target: 'node',	 // vscode extensions run in a Node.js-context
+	entry: './src/extension.ts', // the entry point of this extension
+	output: {	// the bundle is stored in the 'dist' folder (check package.json)
+        filename: 'extension.js',
+        libraryTarget: "commonjs2"
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed
 	},
 	resolve: { // support reading TypeScript and JavaScript files
 		extensions: ['.ts', '.js']
 	},
-	node: {
-		__dirname: false, // leave the __dirname-behaviour intact
-	},
 	module: {
-		rules: [{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [{
-				// configure TypeScript loader:
-				// * enable sources maps for end-to-end source maps
-				loader: 'ts-loader',
-				options: {
-					compilerOptions: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    compilerOptions: {
 						'sourceMap': true,
 						'declaration': false
-					}
-				}
-			}]
-		}]
-	},
-	externals: {
-		vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed
-	},
-	output: {
-		libraryTarget: 'commonjs2',
-		devtoolModuleFilenameTemplate: "../../[resource-path]"
-	},
-	devtool: 'source-map'
-}
+                    }
+                }
+            }]
+        }]
+    }
+};
--- build/web-extension.webpack.config.js
+++ build/web-extension.webpack.config.js
@@ -8,18 +8,22 @@
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
-
-const path = require('path');
-
 module.exports = /** @type WebpackConfig */ {
-	context: path.dirname(__dirname),
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
-		extension: './src/web-extension.ts',
+		extension: './src/web-extension.ts',	// source of the web extension main file
+	},
+	output: {
+		filename: 'web-extension.js',
+		libraryTarget: 'commonjs'
+	},
+	devtool: 'nosources-source-map', // create a source map that points to the original source file
+	externals: {
+		'vscode': 'commonjs vscode', // ignored because it doesn't exist
 	},
 	resolve: {
-		mainFields: ['module', 'main'],
+		mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
 		extensions: ['.ts', '.js'], // support ts-files and js-files
 		fallback: {
 			events: require.resolve('events'),
@@ -28,32 +32,27 @@ module.exports = /** @type WebpackConfig */ {
 		}
 	},
 	module: {
-		rules: [{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [{
-				// configure TypeScript loader:
-				// * enable sources maps for end-to-end source maps
-				loader: 'ts-loader',
-				options: {
-					compilerOptions: {
-						'sourceMap': true,
-						'declaration': false
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						// configure TypeScript loader:
+						// * enable sources maps for end-to-end source maps
+						loader: 'ts-loader',
+						options: {
+							compilerOptions: {
+								'sourceMap': true,
+								'declaration': false
+							}
+						}
 					}
-				}
-			}]
-		}]
-	},
-	externals: {
-		'vscode': 'commonjs vscode', // ignored because it doesn't exist
+				]
+			}
+		]
 	},
 	performance: {
 		hints: false
-	},
-	output: {
-		filename: 'web-extension.js',
-		path: path.join(__dirname, '../dist'),
-		libraryTarget: 'commonjs'
-	},
-	devtool: 'source-map'
+	}
 };
